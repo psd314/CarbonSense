6610e012ee4fc57d7678357129fe4dafdd63bdd8
DESKTOP-U6KB5LI\march <marchettipm@gmail.com>
2017-11-14 21:04:41 -0500

    first

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -6,20 +6,19 @@ class Nav extends Component {
 
 	render() {
 		return (
-			<ul className="nav">
-			  <li className="nav-item">
-			    <a className="nav-link active" href="/login">Login</a>
-			  </li>
-			  <li className="nav-item">
-			    <a className="nav-link" href="/dashboard">Dashboard</a>
-			  </li>
-			  <li className="nav-item">
-			    <a className="nav-link" href="/leaderboard">Leaderboard</a>
-			  </li>
-			  <li className="nav-item">
-			    <a className="nav-link" href="/profile">Profile</a>
-			  </li>
-			</ul>
+		<nav className="navbar navbar-expand-lg" id="nav" style={{ backgroundColor: "#29c7d8"}}>
+  			<a className="navbar-brand mr-auto">CarbonSense</a>
+ 		 	<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    			<span className="navbar-toggler-icon"></span>
+  			</button>
+  			<div className="collapse navbar-collapse p-0" id="navbarNavAltMarkup">
+    			<div className="navbar-nav">
+      				<button type="button" class="btn btn-outline-success">Dashboard <span className="sr-only">(current)</span></button>
+      				<button type="button" class="btn btn-outline-success">Profile <span className="sr-only">(current)</span></button>
+      				<button type="button" class="btn btn-outline-success">Leaderboard <span className="sr-only">(current)</span></button>
+    			</div>
+  			</div>
+		</nav>
 		);
 	}
 }
