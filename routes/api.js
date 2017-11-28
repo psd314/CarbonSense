73503e6a57fa8fe5a450f49ede9053102924e478
Philippe Dixon <pdixon555@hotmail.com>
2017-11-28 18:20:46 -0500

    additional route added

--- routes/api.js
+++ routes/api.js
@@ -88,7 +88,7 @@ router.route('/login')
         //will need to hash and salt for production, bcrypt???
         const password = req.body.password;
 
-        
+        console.log('dir', __dirname);
         res.json("login route worked");
     });
 
