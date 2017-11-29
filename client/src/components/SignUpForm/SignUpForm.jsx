d630c0469216641f506d14b0e368b52f9a48a38a
Philippe Dixon <pdixon555@hotmail.com>
2017-11-29 16:55:58 -0500

    add signup page

---
+++ client/src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class SignUpForm extends Component {
+	
+	
+	render() {
+		return(
+			<div style={{paddingTop: 150, color: "white"}}>
+				<h1>Sign Up</h1>
+
+				<form>
+				  <div className="form-group col-4 offset-4">
+				    <label htmlFor="email">Email</label>
+				    <input 
+				    	type="email" 
+				    	className="form-control" 
+				    	id="email" 
+				    	placeholder="myname@email.com" 
+				    	value={this.props.value}
+				    	onChange={this.props.handleInputChange}
+				    	name="name" />
+				  </div>
+				  <div className="form-group col-4 offset-4">
+				    <label htmlFor="password">Password</label>
+				    <input 
+				    	type="text" 
+				    	className="form-control" 
+				    	id="password"  
+				    	placeholder="********"
+				    	value={this.props.value}
+				    	onChange={this.props.handleInputChange}
+				    	name="password"/>
+				  </div>
+
+				  <div className="form-group col-4 offset-4">
+				    <label htmlFor="confirmPassword">Confirm Password</label>
+				    <input 
+				    	type="password" 
+				    	className="form-control" 
+				    	id="confirmPassword" 
+				    	placeholder="********"
+				    	value={this.props.value}
+				    	onChange={this.props.handleInputChange}
+				    	name="confirmPassword"/>
+				  </div>			 
+				</form>
+		    	<button onClick={this.props.handleFormSubmit} className="btn btn-lg btn-warning">Sign Up</button>
+			</div>
+		);
+	}
+}
+
+export default SignUpForm;
