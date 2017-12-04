35249f985fed8bed132aba75ba4d06765e030487
Philippe Dixon <pdixon555@hotmail.com>
2017-12-03 20:05:04 -0500

    profile editing and loading

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -19,29 +19,19 @@ componentDidMount() {
 			});
 	}
 
-
-
 	render() {
 		return(
-<div className="container-fluid well span6">
-	<div className="row-fluid">
-        
-        <div className="span8">
-            <ProfileInfo 
-            		alttext={this.state.user.name}
-					src={this.state.user.image}
-					username={this.state.user.name}
-					location={this.state.user.location}
-					gender={this.state.user.gender}
-					successStreak={this.state.user.successStreak}
-					challengeScore={this.state.user.challengeScore}
-            />
-
-        </div>
-        
-        
-	</div>
-</div>
+			<div className="container-fluid well span6">
+				<div className="row-fluid">        
+			        <div className="span8">
+			            <ProfileInfo 
+			            		alttext={this.state.user.name}
+								successStreak={this.state.user.successStreak}
+								challengeScore={this.state.user.challengeScore}
+			            />
+			        </div>       
+				</div>
+			</div>
 		);
 	}
 }
