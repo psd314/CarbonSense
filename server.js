a7b6fd3d1c915453427c8d8fd2d0b4e78df7aaae
Philippe Dixon <pdixon555@hotmail.com>
2017-12-05 20:12:04 -0500

    adjust mongo uri

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/carbonsensedb" || "mongodb://heroku_srkg42s5:egs9f46s3i5n7q17c7ul3vb2r1@ds113046.mlab.com:13046/heroku_srkg42s5",
+  process.env.MONGODB_URI || "mongodb://localhost/carbonsensedb" || "mongodb://heroku_1nql9ckg:suvfcktps2gv80vf0khalm987t@ds131826.mlab.com:31826/heroku_1nql9ckg",
   {
     useMongoClient: true
   }
